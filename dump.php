8babeddee20e25c042c14c4c62758f60d868c4e9
Brian Andhika Putra <135279979+brianandhikap@users.noreply.github.com>
2025-07-14 15:17:50 +0700

    Update dump.php

--- dump.php
+++ dump.php
@@ -9,7 +9,7 @@ if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
 $error = '';
 
 $valid_users = [
-    '***' => '***',
+    'admin' => 'user!@#',
 ];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
